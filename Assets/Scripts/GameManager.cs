7d06064bbe0ee11a0af964c6af829f129aa063ee
andrewrines <andrew.rines@loop.colum.edu>
2015-11-13 15:44:53 -0600

    Added a few scripts.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour 
+{
+	Vector3 SpawnPosition;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		SpawnPosition = GameObject.FindGameObjectWithTag("Spawn").transform.position;
+		Instantiate (Resources.Load("Art/Player"), SpawnPosition, Quaternion.identity);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		
 	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
+	public float moveSpeed = 5f;
 
-    }
-
+	void Start()
+	{
+		
+	}
+	
 	void Update ()
-    {
-        
+	{
+		if (Input.GetKey(KeyCode.A))
+			transform.Translate(-transform.right * Time.deltaTime * moveSpeed);
+		else if (Input.GetKey(KeyCode.D))
+			transform.Translate(transform.right * Time.deltaTime * moveSpeed);
 	}
 }
