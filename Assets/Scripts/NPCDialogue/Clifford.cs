71a9a846c50dbf3ba89193a0ea318e4a64ecd2e2
Dane Wheaton <Dane Wheaton>
2016-01-16 11:04:16 -0600

    Train Game's scene TheWholeFuckingTrain is, as the name implies, the whole game in one scene. Also, Clifford's dialogue is less weird but not completely... not weird.

--- Assets/Scripts/NPCDialogue/Clifford.cs
+++ Assets/Scripts/NPCDialogue/Clifford.cs
@@ -14,6 +14,8 @@ public class Clifford : MonoBehaviour
     void Awake()
     {
         cliff = GameObject.FindGameObjectWithTag("GUI").GetComponent<Text>();
+
+        // I don't like that a separate GUI is used for each choice
         choice1 = GameObject.FindGameObjectWithTag("Choice1GUI").GetComponent<Text>();
         choice2 = GameObject.FindGameObjectWithTag("Choice2GUI").GetComponent<Text>();
         choice3 = GameObject.FindGameObjectWithTag("Choice3GUI").GetComponent<Text>();
@@ -23,71 +25,37 @@ public class Clifford : MonoBehaviour
     {
         if (c.tag == "Player" && Input.GetKeyDown(KeyCode.Return) && Inventory.water == false)
         {
-            // this is all fucked up because coroutines all play at the same time - they execute in order, but they don't wait for the last one to stop for the next one to begin
-            cliff.text = ("Who's there?");
-            StartCoroutine(openDoor());
-            StartCoroutine(mediumWait("Eleanor, it's the porter girl!"));
-            StartCoroutine(longWait("Hello there, young lady. What's the news? Just checking in?"));
-            StartCoroutine(extraLongWait("THIRST WATER GET THIRST THIRST"));
-            StartCoroutine(closeDoor(6.5f));
+            StartCoroutine(FirstConversation());
         }
 
         if (c.tag == "Player" && Input.GetKeyDown(KeyCode.Return) && Inventory.water == true)
         {
-            StartCoroutine(openDoor());
-            StartCoroutine(shortWait("SMOKEWEED"));
-            StartCoroutine(herb());
+            StartCoroutine(BroughtWater());
         }
     }
 
-    IEnumerator openDoor()
+    IEnumerator FirstConversation()
     {
-        yield return new WaitForSeconds(1.5f);
+        cliff.text = "Who's there?";
+        yield return new WaitForSeconds(1);
+        cliff.text = "";
+        yield return new WaitForSeconds(.5f);
         GetComponent<Renderer>().enabled = true;
-    }
-
-    IEnumerator closeDoor(float waitTime)
-    {
-        yield return new WaitForSeconds(6.5f);
+        cliff.text = "Eleanor, it's the porter girl!";
+        yield return new WaitForSeconds(2);
+        cliff.text = "Hello there, young lady. What's the news? Just checking in?";
+        yield return new WaitForSeconds(2.5f);
+        cliff.text = "THIRST WATER GET THIRST THIRST";
+        yield return new WaitForSeconds(1.5f);
         GetComponent<Renderer>().enabled = false;
         cliff.text = "";
     }
 
-    IEnumerator veryShortWait(string dialogue)
+    IEnumerator BroughtWater()
     {
-        yield return new WaitForSeconds(.7f);
-        cliff.text = (dialogue);
-    }
-
-    IEnumerator shortWait(string dialogue)
-    {
+        GetComponent<Renderer>().enabled = true;
+        cliff.text = "SMOKEWEED";
         yield return new WaitForSeconds(1);
-        cliff.text = (dialogue);
-    }
-
-    IEnumerator mediumWait(string dialogue)
-    {
-        yield return new WaitForSeconds(2);
-        cliff.text = (dialogue);
-    }
-
-    IEnumerator longWait(string dialogue)
-    {
-        yield return new WaitForSeconds(3.5f);
-        cliff.text = (dialogue);
-    }
-
-    IEnumerator extraLongWait(string dialogue)
-    {
-        yield return new WaitForSeconds(5.5f);
-        cliff.text = (dialogue);
-    }
-
-
-
-    IEnumerator herb()
-    {
-        yield return new WaitForSeconds(3);
         choice1.text = "weed";
         choice2.text = "marijuana";
         choice3.text = "a bug";
---
+++ Assets/Scripts/PlayerBehaviors/DoorsBetweenCars.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorsBetweenCars : MonoBehaviour
+{
+    void OnTriggerEnter (Collider c)
+    {
+        
+        if (c.tag == "RightDoor")
+        {
+            transform.position += new Vector3(2, 0, 0);
+        }
+        else if (c.tag == "LeftDoor")
+        {
+            transform.position += new Vector3(-2, 0, 0);
+        }
+        else if (c.tag == "DoorToCabin1Car1") // etc.
+        {
+            // for any cabin, simply overlay an image GUI on the existing scene
+        }
+    }
+}
